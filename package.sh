2d56f6ec46dad9996a5ec6ef9ce2fed2e41dceef
Damien Aicheh <damienaicheh@microsoft.com>
2024-08-27 13:49:18 +0000

    feat: Add package script.

---
+++ package.sh
@@ -0,0 +1,13 @@
+echo Build and package..
+
+dotnet restore
+
+dotnet build -c Release
+
+echo Publishing the project..
+
+dotnet publish -c Release -o out
+
+cd out && zip -r app.zip .
+
+pwd app.zip
